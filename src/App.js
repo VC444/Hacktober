47b8b27424bba89d3d28b0a98dadb6e1b0fe3ebb
Michael Ngo <michaelngo@mivsolution.com>
2018-10-27 18:58:12 -0500

    Adjusted port for socket client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Websocket from 'react-websocket';
-import io from 'socket.io';
+import io from 'socket.io-client';
 
 class App extends Component {
 
   componentDidMount = () => {
-    const socket = io('http://localhost:3000/');
+    const socket = io('http://localhost:8000/');
     socket.on('connect', function () { console.log('socket connected') });
-    socket.on('tags', function (data) { console.log('socket tags') });
+    socket.on('tags', function (data) { console.log(data) });
     socket.on('disconnect', function () { console.log('socket disconnected') });
   }
 
