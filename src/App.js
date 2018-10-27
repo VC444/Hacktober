fe1e8e4fa003cd8a16ea203b16e9e3af1b19ae78
Vignesh Chandrasekharan <vchan444@gmail.com>
2018-10-27 17:49:45 -0500

    Created Socket.io connections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Websocket from 'react-websocket';
+import io from 'socket.io';
 
 class App extends Component {
+
+  componentDidMount = () => {
+    const socket = io('http://localhost:3000/');
+    socket.on('connect', function () { console.log('socket connected') });
+    socket.on('tags', function (data) { console.log('socket tags') });
+    socket.on('disconnect', function () { console.log('socket disconnected') });
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>Hello</h4>
       </div>
     );
   }
